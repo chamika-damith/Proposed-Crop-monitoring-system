98717fb180ef93ef49deab87a94ab5e87184f3b6
chamika-damith <chamikadamith9@gmail.com>
2024-10-30 11:09:42 +0530

    create navbar

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// Toggle Mobile Menu
+function toggleMobileMenu() {
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.classList.toggle("hidden");
+}
+
+// Toggle Profile Menu
+function toggleProfileMenu(){
+    const profileMenu = document.getElementById("user-menu-button");
+    profileMenu.classList.toggle("hidden");
+}
